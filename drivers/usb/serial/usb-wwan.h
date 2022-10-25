b2c133b6b74183be8b0681f04d6c86bd206f8b86
ponljung <53216489+ponljung@users.noreply.github.com>
2022-10-25 17:58:24 +0200

    Update usb-wwan.h

--- drivers/usb/serial/usb-wwan.h
+++ drivers/usb/serial/usb-wwan.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Definitions for USB serial mobile broadband cards
  */
@@ -9,8 +10,16 @@ extern void usb_wwan_dtr_rts(struct usb_serial_port *port, int on);
 extern int usb_wwan_open(struct tty_struct *tty, struct usb_serial_port *port);
 extern void usb_wwan_close(struct usb_serial_port *port);
 extern int usb_wwan_port_probe(struct usb_serial_port *port);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,12,0)
+extern void usb_wwan_port_remove(struct usb_serial_port* port);
+#else 
 extern int usb_wwan_port_remove(struct usb_serial_port *port);
-extern int usb_wwan_write_room(struct tty_struct *tty);
+#endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0)
+extern unsigned int usb_wwan_write_room(struct tty_struct *tty);
+#else
+extern int usb_wwan_write_room(struct tty_struct* tty);
+#endif 
 extern int usb_wwan_tiocmget(struct tty_struct *tty);
 extern int usb_wwan_tiocmset(struct tty_struct *tty,
 			     unsigned int set, unsigned int clear);
@@ -18,7 +27,11 @@ extern int usb_wwan_ioctl(struct tty_struct *tty,
 			  unsigned int cmd, unsigned long arg);
 extern int usb_wwan_write(struct tty_struct *tty, struct usb_serial_port *port,
 			  const unsigned char *buf, int count);
-extern int usb_wwan_chars_in_buffer(struct tty_struct *tty);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0)
+extern unsigned int usb_wwan_chars_in_buffer(struct tty_struct *tty);
+#else 
+extern int usb_wwan_chars_in_buffer(struct tty_struct* tty);
+#endif
 #ifdef CONFIG_PM
 extern int usb_wwan_suspend(struct usb_serial *serial, pm_message_t message);
 extern int usb_wwan_resume(struct usb_serial *serial);
@@ -31,13 +44,12 @@ extern int usb_wwan_resume(struct usb_serial *serial);
 #define IN_BUFLEN 4096
 #define OUT_BUFLEN 4096
 
-#define HUAWEI_VENDOR_ID	0x12D1
-#define HW_BCDUSB		0x0110
-
 struct usb_wwan_intf_private {
 	spinlock_t susp_lock;
 	unsigned int suspended:1;
 	unsigned int use_send_setup:1;
+	unsigned int use_zlp:1;
+
 	int in_flight;
 	unsigned int open_ports;
 	void *private;
